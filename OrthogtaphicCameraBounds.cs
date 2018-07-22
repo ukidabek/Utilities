cecf958bef19f4ab79021f4e4c366770d4e07bfc
Ukidabek <ukidabek@gmail.com>
2018-07-22 16:00:23 +0200

    - Camera bounds logic added.

---
+++ OrthogtaphicCameraBounds.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BaseGameLogic.Utilities
+{
+    [Serializable] public class OrthogtaphicCameraBounds
+    {
+        public float MinHeight = 0f;
+        public float MaxHeight = 0f;
+        public float MinWidth = 0f;
+        public float MaxWidth = 0f;
+    }
+}
---
+++ OrthogtaphicCameraBoundsConstraint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseGameLogic.Utilities
+{
+    public class OrthogtaphicCameraBoundsConstraint
+    {
+        private OrthogtaphicCameraBounds _bounds = null;
+        private Camera _camera = null;
+
+        public OrthogtaphicCameraBoundsConstraint(OrthogtaphicCameraBounds bounds, Camera camera)
+        {
+            _bounds = bounds;
+            _camera = camera;
+        }
+    
+        public void ForceBoundaries()
+        {
+            float x = _camera.orthographicSize * _camera.aspect;
+            float y = _camera.orthographicSize;
+
+            Vector3 position = _camera.gameObject.transform.position;
+
+            position.x = MinCheck(position.x, x, _bounds.MinWidth);
+            position.x = MaxCheck(position.x, x, _bounds.MaxWidth);
+            position.y = MinCheck(position.y, y, _bounds.MinHeight);
+            position.y = MaxCheck(position.y, y, _bounds.MaxHeight);
+
+            _camera.gameObject.transform.position = position;
+        }
+
+        private float MinCheck(float x, float x1, float xMin)
+        {
+            if (x - x1 < xMin)
+                return xMin + x1;
+            else
+                return x;
+        }
+
+        private float MaxCheck(float x, float x1, float xMax)
+        {
+            if (x + x1 > xMax)
+                return xMax - x1;
+            else
+                return x;
+        }
+    }
+}
