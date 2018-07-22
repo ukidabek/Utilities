d25802e449bcacd460b2eb748105b002242aec9a
Ukidabek <ukidabek@gmail.com>
2018-07-22 14:30:59 +0200

    - ColorLerp update.

--- ColorLerp.cs
+++ ColorLerp.cs
@@ -7,19 +7,19 @@ namespace BaseGameLogic.Utilities.UI
     {
         private enum Mode { AtoB, BtoA }
         [SerializeField] private Mode _mode = Mode.AtoB;
-        [SerializeField] private Image _image = null;
+        [SerializeField] private Graphic _graphic = null;
         [SerializeField] private Color _a = new Color();
         [SerializeField] private Color _b = new Color();
 
         private void Awake()
         {
-            _image.color = _a;
+            _graphic.color = _a;
         }
 
         public void LerpColor(float fill)
         {
             var t = _mode == Mode.AtoB ? fill : 1 - fill;
-            _image.color = Color.Lerp(_a, _b, t);
+            _graphic.color = Color.Lerp(_a, _b, t);
         }
     }
 }
