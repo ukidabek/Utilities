39e8654dfb3df8c69deb6d2cbeb0318374adf46f
Ukidabek <ukidabek@gmail.com>
2018-07-23 20:15:09 +0200

    - OrthogtaphicCameraBounds constructor update.

--- OrthogtaphicCameraBounds.cs
+++ OrthogtaphicCameraBounds.cs
@@ -7,5 +7,15 @@ namespace BaseGameLogic.Utilities
         public float MaxHeight = 0f;
         public float MinWidth = 0f;
         public float MaxWidth = 0f;
+
+        public OrthogtaphicCameraBounds() {}
+
+        public OrthogtaphicCameraBounds(float minHeight, float maxHeight, float minWidth, float maxWidth)
+        {
+            MinHeight = minHeight;
+            MaxHeight = maxHeight;
+            MinWidth = minWidth;
+            MaxWidth = maxWidth;
+        }
     }
 }
