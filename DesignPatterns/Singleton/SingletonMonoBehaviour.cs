16eba6c2f3d1df55f1d72569fba9c968258cd94a
Ukidabek <ukidabek@gmail.com>
2019-04-26 18:33:56 +0200

    - Singleton base classes added.

---
+++ DesignPatterns/Singleton/SingletonMonoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BaseGameLogic.Singleton
+{
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+    {
+        public static T Instance { get; protected set; }
+
+        [SerializeField] private bool _dontDestroyOnLoad = false;
+
+        protected virtual void CreateInstance()
+        {
+            if (Instance == null)
+                Instance = this as T;
+            else
+            {
+                Debug.LogErrorFormat("GameObject {0} will be destroyed.", gameObject.name);
+                Destroy(gameObject);
+            }
+
+            if(Instance != null && _dontDestroyOnLoad)
+                DontDestroyOnLoad(this.gameObject);
+        }
+
+        protected virtual void Awake()
+        {
+            CreateInstance();
+        }
+
+        protected virtual void Start() {}
+
+        protected virtual void Reset()
+        {
+            gameObject.name = GetType().Name;
+        }
+    }
+}
