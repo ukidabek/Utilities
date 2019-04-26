ac3de338a269ed9eea068cd6e6b04f268cc67369
Ukidabek <ukidabek@gmail.com>
2019-04-26 21:30:02 +0200

    - TypeManager added.

---
+++ CustomTypes/CustomType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseGameLogic.CustomTypes
+{
+    [Serializable]
+    public class CustomType
+    {
+        [SerializeField] private string groupName = string.Empty;
+        [SerializeField] private string typeName = string.Empty;
+    }
+}
