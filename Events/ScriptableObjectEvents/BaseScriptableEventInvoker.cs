1111dfccf540ada39c47efceeadd35767d799854
Łukasz Dąbek <ldabek@artifexmundi.com>
2020-03-31 19:24:26 +0200

    Base class for scriptable event invoker added. Invoker for ScriptableEvent added.

--- Events/ScriptableObjectEvents/BaseScriptableEventInvoker.cs
+++ Events/ScriptableObjectEvents/BaseScriptableEventInvoker.cs
@@ -1,11 +1,24 @@
+using System;
 using UnityEngine;
 
 namespace Utilities.Events
 {
     public abstract class BaseScriptableEventInvoker : MonoBehaviour
     {
+        [SerializeField] private bool m_invokeOnAwake = false;
+
+        private void Awake()
+        {
+            if (m_invokeOnAwake)
+                InvokeOnAwake();
+        }
+
+        protected virtual void InvokeOnAwake()
+        {
+            throw new NotImplementedException();
+        }
     }
-    
+
     public abstract class BaseScriptableEventInvoker<T> : BaseScriptableEventInvoker
     {
         public abstract void Invoke(T value);
