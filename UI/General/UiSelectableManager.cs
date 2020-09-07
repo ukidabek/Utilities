41184133c56d8ed0dc49d9bdcdda62bfac4bd0a4
Łukasz Dąbek <ldabek@artifexmundi.com>
2020-09-07 22:33:30 +0200

    UiSelectableManager can react on hierarchy change.

--- UI/General/UiSelectableManager.cs
+++ UI/General/UiSelectableManager.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 namespace Utilities.General.UI
 {
     public class UiSelectableManager : MonoBehaviour
     {
         [SerializeField] private bool m_autoInitialize = true;
+        [SerializeField] private bool m_reactOnHierarchyChange = false;
         [SerializeField] private Selectable[] m_selectable = null;
 
         private IEnumerator Start()
         {
             if (!m_autoInitialize) yield break;
             yield return null;
+            GetSelectables();
+        }
+        
+        private void GetSelectables()
+        {
             m_selectable = gameObject.GetComponentsInChildren<Selectable>(true);
+        }
+
+        private void OnTransformChildrenChanged()
+        {
+            if(!m_reactOnHierarchyChange) return;
+            GetSelectables();
         }
 
         public void SetStatus(bool isInteractable)
