1e548d68b42a7231d6c8e226d8d0e54a5ee598e4
Łukasz Dąbek <ldabek@artifexmundi.com>
2020-11-08 11:53:01 +0100

    Link opening component added.

---
+++ UI/General/UIOpenLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOpenLink : MonoBehaviour
+{
+    [SerializeField] private string m_link = string.Empty;
+
+    public void OpenLink() => Application.OpenURL(m_link);
+}
