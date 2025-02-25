8f486fdb10b6b2a78053295f6ea52d874afedd54
Łukasz Dąbek <ukidabek@gmail.com>
2025-02-25 21:21:41 +0100

    [Code] Character resource access refactored.

--- Runtime/Characters/Character.cs
+++ Runtime/Characters/Character.cs
@@ -11,35 +11,46 @@ namespace Utilities.General.Characters
 
 		[ReferenceList, SerializeReference] private ICharacterResource[] m_characterResources = null;
 		public IEnumerable<ICharacterResource> Resources => m_characterResources;
+		
+		private Dictionary<Key, ICharacterResource> m_resourcesDictionary = new Dictionary<Key, ICharacterResource>();
 
-		private IIsDeadStatusProvider m_isDeadStatusProvider = null;
-		public bool IsDead => ChandleStatusProvider(m_isDeadStatusProvider);
-
+		[Reference, SerializeReference] private IIsDeadStatusProvider m_isDeadStatusProvider = null;
+		public bool IsDead => HandleStatusProvider(m_isDeadStatusProvider);
+		public bool IsAlive => !IsDead;
+		
 		protected virtual void Awake()
 		{
 			m_isDeadStatusProvider = GetComponent<IIsDeadStatusProvider>();
 			foreach (var resource in m_characterResources)
 			{
-				resource.Reset();
+				m_resourcesDictionary.Add(resource.Key, resource);
+				resource.Initialize();
 			}
 		}
-
+		
 		protected virtual void OnEnable() => m_characterPool?.AddCharacter(this);
 
 		protected virtual void OnDisable() => m_characterPool?.RemoveCharacter(this);
 		
 		public IEnumerable<T> GetResourcesOfType<T>() where T : CharacterResource => m_characterResources.OfType<T>();
 
-		protected bool ChandleStatusProvider<T>(T statusProvider) where T : IStatusProvider
+		public void ResetResources()
 		{
-			if (statusProvider == null)
-			{
-
-				Debug.LogWarning($"Status provider of type <b>{typeof(T).Name}</b> is null.");
-				return false;
-			}
-
-			return statusProvider.Status;
+			foreach (var resource in m_characterResources) 
+				resource.Reset();
+		}
+		
+		public bool TryGetResource(Key key, out ICharacterResource resource) => m_resourcesDictionary.TryGetValue(key, out resource);
+		
+		public bool HaveResource(Key key) => m_resourcesDictionary.ContainsKey(key);
+		
+		public ICharacterResource this[Key key] => m_resourcesDictionary[key]; 
+		
+		public bool HandleStatusProvider<T>(T statusProvider) where T : IStatusProvider
+		{
+			if (statusProvider != null) return statusProvider.Status;
+			Debug.LogWarning($"Status provider of type <b>{typeof(T).Name}</b> is null.");
+			return false;
 		}
 	}
 }
